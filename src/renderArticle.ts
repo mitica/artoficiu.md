d946da7360fc10da59d8f98b77b5bf4c64fcf253
Mitica <canteadumitru@gmail.com>
2018-11-09 11:40:22 +0200

    render article videos

--- src/renderArticle.ts
+++ src/renderArticle.ts
@@ -17,14 +17,36 @@ export default function render(text: string) {
 }
 
 renderer.image = function (href: string, title: string, text: string) {
-    let imageId = '';
-    const result = /images\.contentful\.com\/[a-zA-Z0-9]+\/[a-zA-Z0-9]+\/([a-zA-Z0-9]+)\//.exec(href);
+    let id = '';
+    let media = ''
+    const result = /(images|videos)\.ctfassets\.net\/[a-zA-Z0-9]+\/[a-zA-Z0-9]+\/([a-zA-Z0-9]+)\//.exec(href);
     if (result) {
         // const urlParts = Url.parse(href, false);
         // href = `${urlParts.pathname}?w=650&q=90`;
-        imageId = 'image-' + result[1];
+        media = result[1];
+        id = result[2];
+    } else {
+        return renderImage(id, href, title, text);
     }
+
+    if (media === 'videos') {
+        return renderVideo(id, href, title, text);
+    }
+
+
+    return renderImage(id, href, title, text);
+}
+
+function renderImage(id: string, href: string, title: string, text: string) {
+    id = 'image-' + id;
     title = encodeURIComponent(title || text);
     text = encodeURIComponent(text);
-    return `<p><a class="article_pic_a js-article-image" name="${imageId}" href="#${imageId}" data-id="${imageId}"><img class="article_pic" alt="${text}" src="${href}" /></a></p>`
+    return `<p><a class="article_pic_a js-article-image" name="${id}" href="#${id}" data-id="${id}"><img class="article_pic" alt="${text}" src="${href}" /></a></p>`
+}
+
+function renderVideo(id: string, href: string, title: string, text: string) {
+    id = 'video-' + id;
+    title = encodeURIComponent(title || text);
+    text = encodeURIComponent(text);
+    return `<p><video class="article_video" controls><source src="${href}" /></video></p>`
 }
